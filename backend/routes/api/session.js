b875779f034f4303b6179f1ae2ece10e4cdb9c44
Roosevelt Burden <rooseveltburden@gmail.com>
2023-03-15 11:42:00 -0600

    Add User login backend endpoint

---
+++ backend/routes/api/session.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { setTokenCookie, restoreUser } = require("../../utils/auth");
+const { User } = require("../../db/models");
+const router = express.Router();
+
+// Log in
+router.post("/", async (req, res, next) => {
+  const { credential, password } = req.body;
+
+  const user = await User.login({ credential, password });
+
+  if (!user) {
+    const err = new Error("Login failed");
+    err.status = 401;
+    err.title = "Login failed";
+    err.errors = { credential: "The provided credentials were invalid." };
+    return next(err);
+  }
+
+  setTokenCookie(res, user);
+
+  return res.json({
+    user: user,
+  });
+});
+
+module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,4 +9,12 @@ router.get("/hello/world", (req, res) => {
   res.send("Hello World!");
 });
 
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-Token": csrfToken,
+  });
+});
+
 module.exports = router;
