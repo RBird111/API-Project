8c65c67840a41c3612ac01ba14191288986a7d8b
Roosevelt Burden <rooseveltburden@gmail.com>
2023-04-07 13:40:41 -0600

    Added some bonus stuff from frontend readme

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import * as sessionActions from "../../store/session";
+import OpenModalMenuItem from "./OpenModalMenuItem";
+import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SignupFormModal";
 
-const ProfileButton = ({ user }) => {
+function ProfileButton({ user }) {
   const dispatch = useDispatch();
-
   const [showMenu, setShowMenu] = useState(false);
   const ulRef = useRef();
 
   const openMenu = () => {
     if (showMenu) return;
-
     setShowMenu(true);
   };
 
@@ -28,37 +29,50 @@ const ProfileButton = ({ user }) => {
     return () => document.removeEventListener("click", closeMenu);
   }, [showMenu]);
 
+  const closeMenu = () => setShowMenu(false);
+
   const logout = (e) => {
     e.preventDefault();
-
     dispatch(sessionActions.logout());
+    closeMenu();
   };
 
   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
 
   return (
-    <div className="navigation-div" ref={ulRef}>
-      <button className="navigation" onClick={openMenu}>
-        <i className="fa-sharp fa-regular fa-user navigation" />
+    <div ref={ulRef}>
+      <button onClick={openMenu}>
+        <i className="fa-sharp fa-regular fa-user" />
       </button>
-
-      <ul className={`${ulClassName}`}>
-        <li className="navigation">{user.username}</li>
-
-        <li className="navigation">
-          {user.firstName} {user.lastName}
-        </li>
-
-        <li className="navigation">{user.email}</li>
-
-        <li className="navigation">
-          <button className="navigation" onClick={logout}>
-            Log Out
-          </button>
-        </li>
+      <ul className={ulClassName}>
+        {user ? (
+          <>
+            <li>{user.username}</li>
+            <li>
+              {user.firstName} {user.lastName}
+            </li>
+            <li>{user.email}</li>
+            <li>
+              <button onClick={logout}>Log Out</button>
+            </li>
+          </>
+        ) : (
+          <>
+            <OpenModalMenuItem
+              itemText="Log In"
+              onItemClick={closeMenu}
+              modalComponent={<LoginFormModal />}
+            />
+            <OpenModalMenuItem
+              itemText="Sign Up"
+              onItemClick={closeMenu}
+              modalComponent={<SignupFormModal />}
+            />
+          </>
+        )}
       </ul>
     </div>
   );
-};
+}
 
 export default ProfileButton;
