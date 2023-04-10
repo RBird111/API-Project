5933d0006587a0ba5baaf774ba1e93a2f9838ab4
Roosevelt Burden <rooseveltburden@gmail.com>
2023-04-10 17:38:26 -0600

    have rought template

--- frontend/src/components/AllSpots/index.js
+++ frontend/src/components/AllSpots/index.js
@@ -2,14 +2,21 @@ import { useSelector } from "react-redux";
 
 import SpotCard from "../SpotCard";
 import "./AllSpots.scss";
+import { NavLink } from "react-router-dom";
 
 const AllSpots = () => {
-  const spots = useSelector((state) => state.spots);
+  const spots = useSelector((state) => state.spots.spotList);
 
   return (
     <div className="all-spots">
       {Object.values(spots).map((spot) => (
-        <SpotCard key={spot.id} spot={spot} />
+        <NavLink
+          className={`spot-card-link`}
+          key={spot.id}
+          to={`/spots/${spot.id}`}
+        >
+          <SpotCard spot={spot} />
+        </NavLink>
       ))}
     </div>
   );
