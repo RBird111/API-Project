ce6b633222591d5b65f5c9fa2ec4a8f849b42a8c
Roosevelt Burden <rooseveltburden@gmail.com>
2023-07-06 09:35:54 -0600

    fixing search part 6, last chapter

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -162,7 +162,7 @@ router.post("/search", async (req, res, _next) => {
     .reduce(
       (acc, attr) => {
         acc[Op.or].push({
-          [attr]: sequelize.where(sequelize.fn('LOWER', sequelize.col(attr)), 'LIKE', '%' + query + '%'),
+          [attr]: sequelize.where(sequelize.fn('LOWER', sequelize.col(attr)), 'LIKE', '%' + query.toLowerCase() + '%'),
         });
         return acc;
       },
